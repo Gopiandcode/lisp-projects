1d15e584426ff4ba1a3d7d7631938cbb651d5267
Kiran Gopinathan <zcabkgo@ucl.ac.uk>
2017-09-12 19:01:29 +0000

    added game repl - coerce is cast,

--- LandOfLisp/ch5.lisp
+++ LandOfLisp/ch5.lisp
@@ -23,6 +23,7 @@
 )
 
 (defparameter *location* 'living-room)
+(defparameter *allowed-commands* '(look walk pickup inventory))
 
 (defun describe-location (location nodes)
   (cadr (assoc location nodes))
@@ -92,4 +93,49 @@
 
 (defun inventory ()
   (cons 'items- (objects-at 'body *objects* *object-locations*))
+)
+
+
+(defun game-read ()
+  (let ((cmd (read-from-string
+	       (concatenate 'string "(" (read-line) ")")
+	     )
+       ))
+    (flet ((quote-it (x)
+		     (list 'quote x)
+	  ))
+      (cons (car cmd) (mapcar #'quote-it (cdr cmd)))
+    )
+  )
+)
+
+(defun game-eval (sexp)
+  (if (member (car sexp) *allowed-commands*)
+    (eval sexp)
+    `(i do not know the command ,sexp)
+  )
+)
+
+(defun tweak-text (lst caps lit)
+  (when lst
+    (let ((item (car lst))
+	  (rest (cdr lst)))
+      (cond ((eql item #\space) (cons item (tweak-text rest caps lit)))        ; if space, attach and continue 
+	    ((member item '(#\! #\? #\.)) (cons item (tweak-text rest t lit))) ; if punctuation, capitalize next
+	    ((eql item #\") (tweak-text  rest caps (not lit)))                 ; if quotation, remove
+	    (lit (cons item (tweak-text rest nil lit)))                        ; if lit continue with rest, do no formatting
+	    (caps (cons (char-upcase item) (tweak-text rest nil lit)))         ; if caps upcase value
+	    (t (cons (char-downcase item) (tweak-text rest nil nil)))          ; otherwise downcase
+      )
+    )
+  )
+)
+
+(defun game-print (lst)
+  (princ (coerce (tweak-text (coerce (string-trim "() " (prin1-to-string lst)) 'list) t nil) 'string))
+  (fresh-line)
+)
+
+(defun game-repl ()
+  (loop (game-print (game-eval (game-read))))
 )
